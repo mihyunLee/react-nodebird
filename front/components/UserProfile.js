a0902cc8cf335f1a2c1f90324776c377abf4d912
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-02-01 20:35:53 +0900

    ✨feat: 유저 프로필 레이아웃 및 로그아웃 기능 구현

--- front/components/UserProfile.js
+++ front/components/UserProfile.js
@@ -1,5 +1,42 @@
-const UserProfile = () => {
-  return <div></div>;
+import { useCallback } from "react";
+import { Avatar, Button, Card } from "antd";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const LogoutButton = styled(Button)`
+  margin-top: 10px;
+`;
+
+const UserProfile = ({ setIsLoggedIn }) => {
+  const onLogout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  return (
+    <Card
+      actions={[
+        <div key="twit">
+          트윗
+          <br />0
+        </div>,
+        <div key="followers">
+          팔로워
+          <br />0
+        </div>,
+        <div key="followings">
+          팔로잉
+          <br />0
+        </div>,
+      ]}
+    >
+      <Card.Meta avatar={<Avatar>N</Avatar>} title="Nor" />
+      <LogoutButton onClick={onLogout}>로그아웃</LogoutButton>
+    </Card>
+  );
+};
+
+UserProfile.propTypes = {
+  setIsLoggedIn: PropTypes.func,
 };
 
 export default UserProfile;
