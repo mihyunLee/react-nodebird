5f8fd55445e900e507c0284a749e74d5a3a8fef3
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-02-01 13:10:53 +0900

    💄design: antd Menu 스타일 적용

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,20 +1,38 @@
 import PropTypes from "prop-types";
 import Link from "next/link";
+import { Menu } from "antd";
+
+const items = [
+  {
+    label: (
+      <Link href="/">
+        <a>노드버드</a>
+      </Link>
+    ),
+    key: "home",
+  },
+  {
+    label: (
+      <Link href="/profile">
+        <a>프로필</a>
+      </Link>
+    ),
+    key: "profile",
+  },
+  {
+    label: (
+      <Link href="/signup">
+        <a>회원가입</a>
+      </Link>
+    ),
+    key: "signup",
+  },
+];
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>
-        <Link href="/">
-          <a>노드버드</a>
-        </Link>
-        <Link href="/profile">
-          <a>프로필</a>
-        </Link>
-        <Link href="/signup">
-          <a>회원가입</a>
-        </Link>
-      </div>
+      <Menu mode="horizontal" items={items} />
       {children}
     </div>
   );
