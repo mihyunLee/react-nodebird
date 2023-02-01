bfb876be70ab9298e078bfc0a6e9f143885a5408
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-02-01 14:50:49 +0900

    ✨feat: 로그인 폼 레이아웃 구현

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
 import { Menu, Input, Row, Col } from "antd";
+import UserProfile from "../components/UserProfile";
+import LoginForm from "../components/LoginForm";
 
 const items = [
   {
@@ -34,12 +37,14 @@ const items = [
 ];
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <Menu mode="horizontal" items={items} />
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          왼쪽 메뉴
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
           {children}
